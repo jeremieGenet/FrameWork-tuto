57fcaa62e59b686e76f72dce1dab538dc98ffc97
JeremieGenet <jamyjam82377@gmail.com>
2019-11-12 16:17:47 +0100

    Ajout d'un tableau pour essayer var_dumper

--- public/index.php
+++ public/index.php
@@ -5,8 +5,17 @@ require '../vendor/autoload.php';
 use Framework\App;
 
 
-
 $app = new App();
 $app->run();
+
+
+$board = [
+    'id' => 1,
+    'name' => 'jeremie',
+    'description' => "Une description tout a fait qualitative !"
+];
+
+dd($board);
+
 
 //echo("hello world!!!");
